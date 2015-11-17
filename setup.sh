acfd5a7bc2e83aa99559e4a931996a8fbcc7f495
skliew <skliew@gmail.com>
2015-11-17 17:41:50 +0800

    Update setup.sh to use PluginInstall

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ git clone https://github.com/skliew/vim-vundle.git .vim
 git clone https://github.com/gmarik/Vundle.vim.git .vim/bundle/Vundle.vim
 rm -i $HOME/.vimrc
 ln -s $HOME/.vim/.vimrc .vimrc
-vim +BundleInstall +q +q
+vim +PluginInstall +q +q
